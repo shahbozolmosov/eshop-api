b59a78bd185715de2afa67d3cbefc40be63625ee
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-24 11:50:27 +0500

    change auth route

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/customers')->group(function () {
-    Route::get('/', [AuthController::class, 'register']);
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::prefix('/customer')->group(function () {
+    Route::post('/', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::put('/', [AuthController::class, 'updateUser'])->middleware('auth:sanctum');
     Route::put('/password', [AuthController::class, 'updatePassword'])->middleware('auth:sanctum');
     Route::get('/account', [AuthController::class, 'account'])->middleware('auth:sanctum');
