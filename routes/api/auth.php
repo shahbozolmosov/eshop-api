a57cdd1e71e0762705f6520b29d9c724c626fb07
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 08:08:01 +0500

    move middleware routes move to group

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -6,7 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/customer')->group(function () {
     Route::post('/', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::put('/', [AuthController::class, 'updateUser'])->middleware('auth:sanctum');
-    Route::put('/password', [AuthController::class, 'updatePassword'])->middleware('auth:sanctum');
-    Route::get('/account', [AuthController::class, 'account'])->middleware('auth:sanctum');
+
+    Route::group(['prefix' => '/', 'middleware' => ['auth:sanctum']], function () {
+        Route::put('/', [AuthController::class, 'updateUser']);
+        Route::put('/password', [AuthController::class, 'updatePassword']);
+        Route::get('/account', [AuthController::class, 'account']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 });
