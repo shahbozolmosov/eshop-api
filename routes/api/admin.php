d4f092aa58bdb7645501008bf7ec1d400e5cb7fe
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 13:46:35 +0500

    add category routes

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -1,1 +1,10 @@
 <?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/admin')->group(function (){
+    Route::apiResources([
+        'categories' => CategoryController::class
+    ]);
+});
