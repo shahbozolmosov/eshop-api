c53e2eac76a94d3a6c5825f526eccc1a6fb67af4
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 17:57:01 +0500

    category controller move to admin folder

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Http\Resources\CategoryResource;
