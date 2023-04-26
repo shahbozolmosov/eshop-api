4f81efb14b85c8b69c36d6133500f59166415d05
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 15:06:49 +0500

    add image to category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'parent_id'];
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 
     public function parentCategory(): BelongsTo
     {
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Image;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -24,31 +25,43 @@ class CategorySeeder extends Seeder
             'parent_id' => 1
         ]);
 
+        $image = new Image(['url' => 'category-images/shirt.png']);
+        $category->images()->save($image);
+
         $category = Category::create([
             'name' => 'Man Work Equipment',
             'parent_id' => 1
         ]);
+        $image = new Image(['url' => 'category-images/man-bag.png']);
+        $category->images()->save($image);
 
         $category = Category::create([
             'name' => 'Man Shoes',
             'parent_id' => 1
         ]);
+        $image = new Image(['url' => 'category-images/man-shoes.png']);
+        $category->images()->save($image);
 
         /* FOR WOMAN SEEDER */
         $category = Category::create([
             'name' => 'Dress',
             'parent_id' => 2
         ]);
-
+        $image = new Image(['url' => 'category-images/fashion.png']);
+        $category->images()->save($image);
 
         $category = Category::create([
             'name' => 'Woman Bag',
             'parent_id' => 2
         ]);
+        $image = new Image(['url' => 'category-images/woman-bag.png']);
+        $category->images()->save($image);
 
         $category = Category::create([
             'name' => 'High Heels',
             'parent_id' => 2
         ]);
+        $image = new Image(['url' => 'category-images/woman-shoes.png']);
+        $category->images()->save($image);
     }
 }
