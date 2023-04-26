4f60857cda9d1c61f77a7c25aedeed085cd41dad
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 08:59:14 +0500

    add check password action to Auth service

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -10,8 +10,17 @@ class AuthService
     public function checkCredentials($request): void
     {
         $credentials = $request->only(['email', 'password']);
-        if(!auth()->attempt($credentials)){
+        if (!auth()->attempt($credentials)) {
             abort(422, 'The provided credentials are incorrect.');
+        }
+    }
+
+    public function checkPassword($currentPassword, $requestPassword): void
+    {
+        if (!Hash::check($requestPassword, $currentPassword)) {
+            throw ValidationException::withMessages([
+                "password"=>'The provided password is incorrect'
+            ]);
         }
     }
 }
