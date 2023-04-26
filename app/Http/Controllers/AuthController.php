89779b10a31c2224d791a17cfec696160629b5db
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 08:28:52 +0500

    done user register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\Role;
 use App\Models\User;
 use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterUserRequest $request): JsonResponse
     {
-        //
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $role = Role::find(2);
+        $token = $user->createToken($user->name, ['ability:customer'])->plainTextToken;
+
+        return $this->return_success([
+            'role' => $role->name,
+            'token' => $token,
+        ], 'Register success');
     }
 
     public function login(LoginUserRequest $request, AuthService $authService): JsonResponse
@@ -27,13 +42,11 @@ class AuthController extends Controller
         $role = $user->role->name;
         // Create a token by ability
         if ($role === 'admin') $token = $user->createToken($user->name)->plainTextToken;
-        else $token = $user->createToken($user->name, ['abilities:customer'])->plainTextToken;
+        else $token = $user->createToken($user->name, ['ability:customer'])->plainTextToken;
 
         return $this->return_success([
             'role' => $role,
             'token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => ''
         ], 'Login success');
     }
 
---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password'
+        ];
+    }
+}
