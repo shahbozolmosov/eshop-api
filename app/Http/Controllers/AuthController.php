68428b3845ddfeacf9b2b66de26202637732fe71
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-26 08:08:38 +0500

    login authenticate done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Models\User;
+use App\Services\AuthService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -12,9 +16,25 @@ class AuthController extends Controller
         //
     }
 
-    public function login()
+    public function login(LoginUserRequest $request, AuthService $authService): JsonResponse
     {
-        //
+        // Check User Credentials
+        $authService->checkCredentials($request);
+
+        //Get Authenticated User
+        $user = auth()->user();
+
+        $role = $user->role->name;
+        // Create a token by ability
+        if ($role === 'admin') $token = $user->createToken($user->name)->plainTextToken;
+        else $token = $user->createToken($user->name, ['abilities:customer'])->plainTextToken;
+
+        return $this->return_success([
+            'role' => $role,
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => ''
+        ], 'Login success');
     }
 
     public function updateUser()
@@ -27,8 +47,15 @@ class AuthController extends Controller
         //
     }
 
-    public function account()
+    public function account(): JsonResponse
     {
-        //
+
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return $this->return_success('', 'Log out success!');
     }
 }
