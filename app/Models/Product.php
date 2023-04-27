15bf512430f517c8eee964d5ead2b46267196829
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-27 10:36:37 +0500

    add image to product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['category_id', 'name', 'price', 'description'];
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 
     public function category(): BelongsTo
     {
@@ -22,4 +28,5 @@ class Product extends Model
     {
         return $this->hasOne(Stock::class);
     }
+
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -11,6 +12,6 @@ class ProductSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::factory(10)->has(Stock::factory(1))->create();
+        Product::factory(10)->hasImages(6)->has(Stock::factory(1))->create();
     }
 }
