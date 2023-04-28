71003395a37c2d82ac643823d84e389dc55374da
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-28 12:03:15 +0500

    rename district resource name

--- app/Http/Resources/DistrictResource.php
+++ app/Http/Resources/DistrictResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DistrictResoure extends JsonResource
+class DistrictResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
