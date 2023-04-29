0e5849e8bdc72fbfec04abd7c2d944b9514227f9
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-29 15:00:21 +0500

    update favorite seeder

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -10,6 +10,31 @@ class FavoriteSeeder extends Seeder
 {
     public function run(): void
     {
-        Favorite::factory(5)->create();
+        $favorites = [
+            [
+                'user_id' => 2,
+                'product_id' => 1
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 2
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 3
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 4
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 5
+            ],
+        ];
+
+        collect($favorites)->map(function ($favorite){
+           Favorite::create($favorite);
+        });
     }
 }
