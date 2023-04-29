7da5b90407779a3b4a667a5d41934ffd53768c7e
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-29 10:07:22 +0500

    added user favorite and favorite relationship to user, product

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -39,5 +40,10 @@ class User extends Authenticatable
     public function isAdmin(): bool
     {
         return $this->role->name === 'admin';
+    }
+
+    public function favorites(): HasMany
+    {
+        return $this->hasMany(Favorite::class);
     }
 }
