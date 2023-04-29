4f85e47ebf6b492d01f15d3dddd34bda461897d0
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-29 14:39:54 +0500

    added shopping cart request validation

---
+++ app/Http/Requests/StoreCartRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCartRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|exists:product,id',
+            'qty' => 'required|string'
+        ];
+    }
+}
