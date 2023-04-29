e59230fc84f3e48290d7d9632dc5815647884a74
shahboz007 <olmasovshahboz007@gmail.com>
2023-04-29 15:09:15 +0500

    added cart seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RegionSeeder::class,
             DistrictSeeder::class,
             FavoriteSeeder::class,
+            CartSeeder::class,
         ]);
     }
 }
