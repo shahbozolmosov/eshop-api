2dbf7ddeff631369feb44a4a1e0caa0ec77e9e47
shahboz007 <olmasovshahboz007@gmail.com>
2023-05-02 17:46:53 +0500

    added product with order relationship done

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'total_price'];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)
+            ->withPivot(['qty', 'price']);
+    }
+}
