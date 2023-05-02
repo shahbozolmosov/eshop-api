4e83941d6eb001d2c8bbec7570ac35366b08dba0
shahboz007 <olmasovshahboz007@gmail.com>
2023-05-02 17:46:03 +0500

    add checkout route

--- routes/api/frontend.php
+++ routes/api/frontend.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\frontend\ProductController;
 use App\Http\Controllers\frontend\LocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/categories')->group(function (){
+Route::prefix('/categories')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);
     Route::get('{category}', [CategoryController::class, 'show']);
 });
@@ -21,12 +21,14 @@ Route::prefix('/products')->group(function () {
 Route::get('/regions', [LocationController::class, 'regions']);
 Route::get('/districts', [LocationController::class, 'districts']);
 
-Route::group(['middleware' => 'auth:sanctum'], function (){
-    Route::prefix('/carts')->group(function (){
-       Route::get('/', [CartController::class, 'index']);
-       Route::get('/{cart}', [CartController::class, 'show']);
-       Route::post('/', [CartController::class, 'store']);
-       Route::delete('/{cart}', [CartController::class, 'destroy']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/checkout', [CartController::class, 'checkout']);
+
+    Route::prefix('/carts')->group(function () {
+        Route::get('/', [CartController::class, 'index']);
+        Route::get('/{cart}', [CartController::class, 'show']);
+        Route::post('/', [CartController::class, 'store']);
+        Route::delete('/{cart}', [CartController::class, 'destroy']);
     });
 
     Route::apiResources([
