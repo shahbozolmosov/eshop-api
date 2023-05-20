091c2931cd21c2e0b79defe8330ba10d3f607c83
shahboz007 <olmasovshahboz007@gmail.com>
2023-05-20 15:50:34 +0500

    rename order resource for get all order

--- app/Http/Resources/OrderGetAllResource.php
+++ app/Http/Resources/OrderGetAllResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class OrderResource extends JsonResource
+class OrderGetAllResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
@@ -13,9 +13,10 @@ class OrderResource extends JsonResource
             'id' => $this->id,
             'address' => json_decode($this->address),
             'total_price' => $this->total_price,
-            'products' => OrderProductResource::collection($this->products),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'products' => $this->products_count,
+            'order_status' => 'shipping',
+            'created_at' => $this->created_at->isoFormat('MMM, D YYYY'),
+            'updated_at' => $this->updated_at->isoFormat('MMM, D YYYY'),
         ];
     }
 }
