76c8a3001a00d2240de75db9b683b28fc6b9f448
shahboz007 <olmasovshahboz007@gmail.com>
2023-05-22 05:18:27 +0500

    change order resource

--- app/Http/Resources/OrderGetAllResource.php
+++ app/Http/Resources/OrderGetAllResource.php
@@ -13,7 +13,7 @@ class OrderGetAllResource extends JsonResource
             'id' => $this->id,
             'address' => json_decode($this->address),
             'total_price' => $this->total_price,
-            'products' => $this->products_count,
+            'products_count' => $this->products_count,
             'order_status' => $this->orderStatus->status,
             'created_at' => $this->created_at->isoFormat('MMM, D YYYY'),
             'updated_at' => $this->updated_at->isoFormat('MMM, D YYYY'),
--- app/Http/Resources/OrderGetSingleResource.php
+++ app/Http/Resources/OrderGetSingleResource.php
@@ -14,6 +14,7 @@ class OrderGetSingleResource extends JsonResource
             'products' => ProductResource::collection($this->products),
             'address' => json_decode($this->address),
             'total_price' => $this->total_price,
+            'order_status' => $this->orderStatus->status,
             'created_at' => $this->created_at->isoFormat('MMM, D YYYY'),
             'updated_at' => $this->updated_at->isoFormat('MMM, D YYYY'),
         ];
